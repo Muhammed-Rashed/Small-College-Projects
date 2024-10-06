66e7046fc4787539b2e76545c19fe645005d5103
Mohamed Rashed <mohammed.games56ali@gmail.com>
2024-10-06 20:50:05 +0300

    Added the last error handling code and made the code look more clean

--- A1_T1_20230335/main.cpp
+++ A1_T1_20230335/main.cpp
@@ -1,50 +1,235 @@
+// File: A1_T1_20230335.cpp
+// Purpose: ………
+// Author: Mohamed Rashed Ali
+// Section: Yet to be determined
+// ID: 20230335
+// TA: Yet to be determined
+// Date: 8 Oct 2024
+
+
+
 #include <iostream>
 #include <vector>
 #include <fstream>
 #include <algorithm>
 
 using namespace std;
+// These are all the functions used for validating and error handling
 
-// Problem 3
+// This small function will be used in error handling by checking if what we entered is an int (It was a nice trick I learned)
+    // Used in problem 6a and 9
+        int validINT()
+        {
+            int num;
+            while (true)
+            {
+                cin >> num;
+                if (cin.fail())
+                {
+                    cout << "Invalid input. Please enter a valid integer: ";
+                    cin.clear();
+                    cin.ignore(1000, '\n');
+                }
+                else
+                {
+                    cin.ignore(1000, '\n');
+                    return num;
+                }
+            }
+        }
+
+// To check if the string we entered is binary or not
+    // Used for problem 6b
+        bool isBinary(const string& prefix)
+        {
+            for (char c : prefix)
+            {
+                if (c != '0' && c != '1')
+                    return false;
+            }
+            return true;
+        }
+
+// This will have all the functions used for each problem
 
 // Function to help find the delimiter by checking if it's not a number nor a word
-string findDelimiter(const string& target)
-{
-    for (char c : target)
-    {
-        if (!isalnum(c))
-            return string(1, c);
-    }
-    return "";
-}
+    // Used for problem 3
+        string findDelimiter(const string& target)
+        {
+            for (char c : target)
+            {
+                if (!isalnum(c))
+                    return string(1, c);
+            }
+            return "";
+        }
 
 // Function to help split the words
-vector<string> split(const string& target)
-{
-    string delimiter = findDelimiter(target);
+        vector<string> split(const string& target)
+        {
+            string delimiter = findDelimiter(target);
 
-    if (delimiter.empty())
-        return { target };
+            if (delimiter.empty())
+                return { target };
 
-    vector<string> result;
-    int start = 0;
-    int end = target.find(delimiter);
+            vector<string> result;
+            int start = 0;
+            int end = target.find(delimiter);
 
-    while (end != -1)
-    {
-        result.push_back(target.substr(start, end - start));
+            while (end != -1)
+            {
+                result.push_back(target.substr(start, end - start));
 
-        start = end + delimiter.length();
+                start = end + delimiter.length();
 
-        end = target.find(delimiter, start);
-    }
+                end = target.find(delimiter, start);
+            }
 
-    result.push_back(target.substr(start));
+            result.push_back(target.substr(start));
 
-    return result;
-}
+            return result;
+        }
 
-// Final code for problem 3
+
+// Function to find binary (IDK why you made us do this it was super easy, but I guess good practice)
+    // Used for problem 6a
+        static void binaryPrint(int n)
+        {
+            // The test case as told in the pdf
+            if (n == 0)
+            {
+                cout << 0;
+                return;
+            }
+
+            // The recursion part
+            if (n > 1)
+                binaryPrint(n / 2);
+
+            cout << (n % 2);
+        }
+
+
+
+// Function for getting the all the different binary cases
+    // Used for problem 6b
+        void numbers(string prefix, int k)
+        {
+            if (k == 0)
+                cout << prefix << endl;
+
+            else
+            {
+                numbers(prefix + "0", k - 1);
+                numbers(prefix + "1", k - 1);
+            }
+        }
+
+
+// It's nice that the Professor made us use dynamic programing although this is a very simple one
+// The function used for calculating the bears and checking
+    // Used for problem 9
+        static bool bears(int n)
+        {
+            // Check if its 42
+            if (n == 42)
+                return true;
+
+            // Check if it's less than 42
+            if (n < 42)
+                return false;
+
+            // Checking while at the same time using the first rule
+            if (n % 2 == 0 && bears(n / 2))
+                return true;
+
+            // Checking while at the same time using the 2nd rule
+            if (n % 3 == 0 || n % 4 == 0)
+            {
+                int lastDigit = n % 10;
+                int secondLastDigit = (n % 100) / 10;
+                int product = lastDigit * secondLastDigit;
+
+                if (product > 0 && bears(n - product))
+                    return true;
+            }
+
+            // Same like the above if conditions
+            if (n % 5 == 0 && bears(n - 42))
+                return true;
+
+
+            // If none of these work then simply false
+            return false;
+        }
+
+
+// Function to convert a string to lowercase
+// (Because some files will have capital letters that the code wouldn't be able to read)
+    // Used for problem 12 (He just had to make us suffer now did he?)
+        string toLowerCase(const string& str)
+        {
+            string result = str;
+            transform(result.begin(), result.end(), result.begin(), ::tolower);
+            return result;
+        }
+
+        // Structure to hold the term and its associated point value
+        struct PhishingTerm {
+            string term;
+            int pointValue;
+        };
+
+// Function to scan the file for phishing terms and calculate the phishing score (Most of the work)
+        void scanFile(const string& fileName, const vector<PhishingTerm>& terms)
+        {
+            ifstream file(fileName);
+
+            // Simple error message
+            if (!file.is_open())
+            {
+                cerr << "Could not open the file: " << fileName << endl;
+                return;
+            }
+
+            // Array to hold the occurrence count of each phishing term
+            vector<int> occurrences(terms.size(), 0);
+            int totalScore = 0;
+
+            string line;
+            while (getline(file, line))
+            {
+                // Convert the line to lowercase
+                string lowerLine = toLowerCase(line);
+
+                for (size_t i = 0; i < terms.size(); ++i)
+                {
+                    size_t pos = lowerLine.find(terms[i].term);
+                    while (pos != string::npos) {
+                        occurrences[i]++;
+                        totalScore += terms[i].pointValue;
+                        pos = lowerLine.find(terms[i].term, pos + terms[i].term.size());
+                    }
+                }
+            }
+
+            file.close();
+
+            // Output the results
+            cout << "Phishing Scan Results:\n";
+            for (size_t i = 0; i < terms.size(); ++i)
+            {
+                if (occurrences[i] > 0)
+                {
+                    cout << "Term: " << terms[i].term << ", Occurrences: " << occurrences[i]
+                         << ", Points: " << occurrences[i] * terms[i].pointValue << endl;
+                }
+            }
+            cout << "Total Phishing Score: " << totalScore << endl;
+        }
+
+
+// These functions will be used for our inputs and outputs
 int problem_3()
 {
     cout<<"Please Enter anything: "<<endl;
@@ -83,79 +268,29 @@ int problem_3()
     return 0;
 }
 
-
-
-// Problem 6a
-
-// Function to find binary (IDK why you made us do this it was super easy, but I guess good practice)
-static void binaryPrint(int n)
-{
-    // The test case as told in the pdf
-    if (n == 0)
-    {
-        cout << 0;
-        return;
-    }
-
-    // The recursion part
-    if (n > 1)
-        binaryPrint(n / 2);
-
-    cout << (n % 2);
-}
-
-// Where you do the inputs
 int problem_6a()
 {
     cout<<"Please enter the number you want"<<endl;
-    int num; cin>>num;
+    int num = validINT();
     binaryPrint(num);
     return 0;
 }
 
-// problem 6b
-
-// Function for getting the all the different binary cases
-void numbers(string prefix, int k)
-{
-    if (k == 0)
-        cout << prefix << endl;
-
-    else
-    {
-        numbers(prefix + "0", k - 1);
-        numbers(prefix + "1", k - 1);
-    }
-}
-
-// Do I have to tell you what is this for again? It's for the inputs
 int problem_6b()
 {
     int k;
     string prefix;
     bool isValid;
 
-    // Input validation loop for the binary number prefix
-    do{
+    do
+    {
         cout << "Please enter the binary number prefix: ";
         cin >> prefix;
 
-        isValid = true;
+        isValid = isBinary(prefix);
 
-        // Check each character in the input string
-        for (char c : prefix)
-        {
-            if (c != '0' && c != '1')
-            {
-                // Set to false if an invalid character is found
-                isValid = false;
-                cout << "Invalid input. Only 0 and 1 are allowed." << endl;
-
-                // Exit the loop early since we found an invalid character
-                break;
-
-            }
-        }
+        if (!isValid)
+            cout << "Invalid input. Only 0 and 1 are allowed." << endl;
 
     } while (!isValid);
 
@@ -166,124 +301,16 @@ int problem_6b()
     return 0;
 }
 
-
-// Problem 9
-
-// It's nice that the Professor made us use dynamic programing although this is a very simple one
-
-// The function used for calculating the bears and checking
-static bool bears(int n)
-{
-    // Check if its 42
-    if (n == 42)
-        return true;
-
-    // Check if it's less than 42
-    if (n < 42)
-        return false;
-
-    // Checking while at the same time using the first rule
-    if (n % 2 == 0 && bears(n / 2))
-        return true;
-
-    // Checking while at the same time using the 2nd rule
-    if (n % 3 == 0 || n % 4 == 0)
-    {
-        int lastDigit = n % 10;
-        int secondLastDigit = (n % 100) / 10;
-        int product = lastDigit * secondLastDigit;
-
-        if (product > 0 && bears(n - product))
-            return true;
-    }
-
-    // Same like the above if conditions
-    if (n % 5 == 0 && bears(n - 42))
-        return true;
-
-
-    // If none of these work then simply false
-    return false;
-}
-
-// No need to explain why this is here do i? inputs and outputs
 int problem_9()
 {
-    int n;
     cout<<"Please enter the number of bears: ";
-    cin>>n;
+    int n = validINT();
 
     if (bears(n))
         cout<<"Bears("<<n<<") is true"<<endl;
     else
         cout<<"Bears("<<n<<") is false"<<endl;
     return 0;
-}
-
-
-// problem 12 (He just had to make us suffer now did he?)
-
-// Function to convert a string to lowercase
-// (Because some files will have capital letters that the code wouldn't be able to read)
-string toLowerCase(const string& str)
-{
-    string result = str;
-    transform(result.begin(), result.end(), result.begin(), ::tolower);
-    return result;
-}
-
-// Structure to hold the term and its associated point value
-struct PhishingTerm {
-    string term;
-    int pointValue;
-};
-
-// Function to scan the file for phishing terms and calculate the phishing score (Most of the work)
-void scanFile(const string& fileName, const vector<PhishingTerm>& terms)
-{
-    ifstream file(fileName);
-
-    // Simple error message
-    if (!file.is_open())
-    {
-        cerr << "Could not open the file: " << fileName << endl;
-        return;
-    }
-
-    // Array to hold the occurrence count of each phishing term
-    vector<int> occurrences(terms.size(), 0);
-    int totalScore = 0;
-
-    string line;
-    while (getline(file, line))
-    {
-        // Convert the line to lowercase
-        string lowerLine = toLowerCase(line);
-
-        for (size_t i = 0; i < terms.size(); ++i)
-        {
-            size_t pos = lowerLine.find(terms[i].term);
-            while (pos != string::npos) {
-                occurrences[i]++;
-                totalScore += terms[i].pointValue;
-                pos = lowerLine.find(terms[i].term, pos + terms[i].term.size());
-            }
-        }
-    }
-
-    file.close();
-
-    // Output the results
-    cout << "Phishing Scan Results:\n";
-    for (size_t i = 0; i < terms.size(); ++i)
-    {
-        if (occurrences[i] > 0)
-        {
-            cout << "Term: " << terms[i].term << ", Occurrences: " << occurrences[i]
-                 << ", Points: " << occurrences[i] * terms[i].pointValue << endl;
-        }
-    }
-    cout << "Total Phishing Score: " << totalScore << endl;
 }
 
 // This one is slightly different with the list inside since no need to put in a function of its own
